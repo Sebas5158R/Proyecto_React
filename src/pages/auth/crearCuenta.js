926ae9ca8f8c061cfdc970b97d90ca0f67e01736
John Doe <johndoe@example.com>
2023-09-07 11:27:20 -0500

    Login in process

--- src/pages/auth/crearCuenta.js
+++ src/pages/auth/crearCuenta.js
@@ -1,102 +1,176 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import APIInvoke from "../../utils/APIInvoke";
+// Sweet alerts
+import swal from "sweetalert";
 
 const CrearCuenta = () => {
+  const [paciente, setPaciente] = useState({
+    full_name: "",
+    email: "",
+    document_type: "",
+    document_number: "",
+    password: "",
+  });
 
-    return (
-      <div class="hold-transition register-page">
-          <div className="register-box">
-  <div className="register-logo">
-    <Link to={"#"}><b>Bienve</b>nido</Link>
-  </div>
-  <div className="card">
-    <div className="card-body register-card-body">
-      <p className="login-box-msg">Registrate, para poder continuar</p>
+  const { full_name, email, document_type, document_number, password } = paciente;
 
-      {/* Form */}
-      <form action="../../index.html" method="post">
+  const onChange = (e) => {
+    setPaciente({
+      ...paciente,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-        {/* Div for full name */}
-        <div className="input-group mb-3">
-          <input type="text" className="form-control" placeholder="Full name" />
-          <div className="input-group-append">
-            <div className="input-group-text">
-              <span className="fas fa-user" />
+  useEffect(() => {
+    document.getElementById("full_name").focus();
+  }, []);
+
+
+  const crearCuenta = async () => {
+    const data = {
+      full_name: paciente.full_name,
+      email: paciente.email,
+      document_type: paciente.document_type,
+      document_number: paciente.document_number,
+      password: paciente.password
+    }
+    const response = await APIInvoke.invokePOST(`/pacientes`, data);
+    console.log(response)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    crearCuenta();
+  };
+
+  return (
+    <div class="hold-transition register-page">
+      <div className="register-box">
+        <div className="register-logo">
+          <Link to={"#"}>
+            <b>Bienve</b>nido
+          </Link>
+        </div>
+        <div className="card">
+          <div className="card-body register-card-body">
+            <p className="login-box-msg">Registrate, para poder continuar</p>
+
+            {/* Form */}
+            <form onSubmit={onSubmit}>
+              {/* Div for full name */}
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Full name"
+                  name="full_name"
+                  id="full_name"
+                  value={full_name}
+                  onChange={onChange}
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <span className="fas fa-user" />
+                  </div>
+                </div>
+              </div>
+
+              {/* Div for email */}
+              <div className="input-group mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={onChange}
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <span className="fas fa-envelope" />
+                  </div>
+                </div>
+              </div>
+
+              {/* Div for document type */}
+              <div className="input-group mb-3">
+                <select name="document_type" onChange={onChange} value={document_type}>
+                  <option>Cedula de ciudadania</option>
+                  <option>Tarjeta de identidad</option>
+                  <option>Pasaporte</option>
+                </select>
+              </div>
+
+              {/* Div for document number */}
+              <div className="input-group mb-3">
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Document number"
+                  name="document_number"
+                  value={document_number}
+                  onChange={onChange}
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <span className="fas fa-user" />
+                  </div>
+                </div>
+              </div>
+
+              {/* Div for password */}
+              <div className="input-group mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={onChange}
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <span className="fas fa-lock" />
+                  </div>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-8">
+                  <div className="icheck-primary">
+                    <input
+                      type="checkbox"
+                      id="agreeTerms"
+                      name="terms"
+                      defaultValue="agree"
+                    />
+                    <label htmlFor="agreeTerms">
+                      I agree to the <Link to={"#"}>terms</Link>
+                    </label>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <button type="submit" className="btn btn-primary btn-block">
+                    Register
+                  </button>
+                </div>
+              </div>
+            </form>
+            <div className="social-auth-links text-center">
+              <p>- OR -</p>
+              <Link to={"/"} className="btn btn-block btn-danger">
+                Cancelar
+              </Link>
             </div>
+            <Link to={"/"} className="text-center">
+              ¿Ya tienes una cuenta?
+            </Link>
           </div>
         </div>
-
-        {/* Div for email */}
-        <div className="input-group mb-3">
-          <input type="email" className="form-control" placeholder="Email" />
-          <div className="input-group-append">
-            <div className="input-group-text">
-              <span className="fas fa-envelope" />
-            </div>
-          </div>
-        </div>
-
-        {/* Div for document type */}
-        <div className="input-group mb-3">
-          <select>
-            <option>Cedula de ciudadania</option>
-            <option>Tarjeta de identidad</option>
-            <option>Pasaporte</option>
-          </select>
-        </div>
-
-        {/* Div for document number */}
-        <div className="input-group mb-3">
-          <input type="number" className="form-control" placeholder="Document number" />
-          <div className="input-group-append">
-            <div className="input-group-text">
-              <span className="fas fa-user" />
-            </div>
-          </div>
-        </div>
-
-        {/* Div for password */}
-        <div className="input-group mb-3">
-          <input type="password" className="form-control" placeholder="Password" />
-          <div className="input-group-append">
-            <div className="input-group-text">
-              <span className="fas fa-lock" />
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-8">
-            <div className="icheck-primary">
-              <input type="checkbox" id="agreeTerms" name="terms" defaultValue="agree" />
-              <label htmlFor="agreeTerms">
-                I agree to the <Link to={"#"}>terms</Link>
-              </label>
-            </div>
-          </div>
-          <div className="col-4">
-            <button type="submit" className="btn btn-primary btn-block">Register</button>
-          </div>
-        </div>
-      </form>
-      <div className="social-auth-links text-center">
-        <p>- OR -</p>
-        <Link to={"#"} className="btn btn-block btn-primary">
-          Registrar
-        </Link>
-        <Link to={"/"} className="btn btn-block btn-danger">
-          Cancelar
-        </Link>
       </div>
-      <Link to={"/"} className="text-center">¿Ya tienes una cuenta?</Link>
     </div>
-  </div>
-</div>
-
-      </div>
-      
-    );
-}
+  );
+};
 
 export default CrearCuenta;
